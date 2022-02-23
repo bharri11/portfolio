31b15ccd22fd1fbc25919457f88c3e306c185591
bharri11 <bharri11@our.ecu.edu.au>
2022-02-23 18:51:47 +1100

    Update Week 2 scripts

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
 
 # Check entered password against a hash of a password stored in a file
+# Simplified version that only checks for the secret.txt file in the current directory
 # Brett Harris 26/1/2022
-
-# Get the directory. If it doesn't exist, ask again
-while true; do
-	read -p "Enter the directory containing the secret.txt file: " folder
-	if [ -d "$folder" ]; then # valid directory
-		break;
-	elif [ "$folder" = "q" ]; then # user typed 'q' to exit
-		exit 0
-	else
-		echo "Invalid directory entered. Please try again or 'q' to quit"
-	fi
-done
+# Updated 1/2/2022
 
 # Get the password (don't echo the input)
-read  -s -p "Type your password: " passwd
+
+echo -e -n "\033[34mType your password: \033[0m"
+read -s passwd
 echo # New line
 
 # hash the password and pass through sed to tidy it up and remove the trailing space and hyphen
 passwd=$(echo "$passwd" | sha256sum | sed 's/ .*//')
 
 # Read the hash of the password from secret.txt.
-read pwdhash < "$folder"/secret.txt
+read pwdhash < ./secret.txt
 
 # compare the hashed password with the hash read from the file
 if [ "$pwdhash" = "$passwd" ]; then
 	# they match:
-	echo "Access Granted"
+	echo -e "\033[32mAccess Granted\033[0m"
 	exit 0
 else
 	# they don't match:
-	echo "Access Denied"
+	echo -e "\033[31mAccess Denied\033[0m"
 	exit 1
 fi
